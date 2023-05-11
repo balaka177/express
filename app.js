38ffb0dd5c4cc1ecf86b38546067915724468b65
balaka177 <122111835+balaka177@users.noreply.github.com>
2023-05-11 12:05:48 +0530

    Dynamic router part 1

--- app.js
+++ app.js
@@ -4,17 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
 
 const app = express();
-
-const Product=require('./models/product');
-const User=require('./models/user');
-const Cart=require('./models/cart');
-const CartItem=require('./models/cart-item');
-
-Product.belongsTo(User);
-User.hasMany(Product);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,51 +16,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-  User.findByPk(1)
-  .then(user => {
-      req.user=user;
-      next();
-  })
-  .catch(err => console.log(err));
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User,{constraints: true,onDelete:'CASCADE'});
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product,{through:CartItem});
-Product.belongsToMany(Cart,{through:CartItem});
-
-app.use(errorController.get404);
-
-sequelize
-// .sync({force:true})
-.sync()
-.then(resp =>{
-  //console.log(resp);
-  return User.findByPk(1);
-})
-.then(user =>{
-  if(!user){
-      return User.create({name:'Vinod',email:'test@gmail.com'});
-      
-  }
-  return user;
-})
-.then(user =>{
-  return user.createCart();
-  
-})
-.then(user =>{
-  app.listen(8080);
-})
-.catch(err =>{
-  console.log(err);
-})
+app.listen(8080);
--- shop.js
+++ shop.js
@@ -1,11 +1,53 @@
-const express=require('express');
+const Product = require('../models/product');
 
-const shop=express.Router();
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
 
-shop.get('/',(req,res,next) => {
-    
-    res.send('<h1>Hello from Express</h1>');
+exports.getProduct=(req,res,next)=>{
+  const prodId=req.params.productId;
+  Product.findById(prodId,product =>{
+    res.render('shop/product-detail',{
+      product:product,
+      pageTitle:product.title,
+      path:'/products'
+    })
+  })
+}
 
-});
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
 
-module.exports=shop;
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
