7e3842d2ab6b07a1ca8ce56f0b5988b125dff1ac
balaka177 <122111835+balaka177@users.noreply.github.com>
2023-05-14 21:44:36 +0530

    Dynamic routes part 2

--- cart.js
+++ cart.js
@@ -1,15 +1,37 @@
-const Sequelize=require('sequelize');
+const { json } = require('body-parser');
+const fs= require('fs');
+const path=require('path');
+const p=path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+module.exports= class Cart{
+   static addProduct(id,productPrice){
+    fs.readFile(p,(err,fileContent) => {
+        let cart={products: [],totalPrice:0}
+        if(!err) {
+            cart=JSON.parse(fileContent);
+        }
 
-const db=require('../util/database');
+        const existngProductIndex=cart.products.findIndex(prod => prod.id===id)
+        const existngProduct=cart.products.find(prod =>prod.id === id);
+        let updatedProduct;
+        if(existngProduct){
+            updatedProduct={...existngProduct};
+            updatedProduct.qty=updatedProduct.qty+1;
+            cart.products=[...cart.products];
+            cart.products[existngProductIndex]=updatedProduct;
+        }
+        else{
+            updatedProduct={id:id,qty:1};
+            cart.products=[...cart.products,updatedProduct];
+        }
+        cart.totalPrice=cart.totalPrice+ +productPrice;
+        fs.writeFile(p,JSON.stringify(cart),err => {
+            console.log(err);
+        })
+    });
+   }
 
-const Cart=db.define('cart',{
-  id:{
-    type:Sequelize.INTEGER,
-    autoIncrement:true,
-    allowNull:false,
-    primaryKey:true,
-  },
-  quantity:Sequelize.INTEGER,
-});
-
-module.exports=Cart;
+}
